64a4a13d851349418bb20702773728205f230ecd
GyewanKim <87048326+larpp@users.noreply.github.com>
2026-06-30 16:14:41 +0900

    Update category_prediction.sh

--- category_prediction.sh
+++ category_prediction.sh
@@ -1,7 +1,7 @@
 # Train
-python -m torch.distributed.run --nproc_per_node=2 --master_port=29505 train.py \
+python -m torch.distributed.run --nproc_per_node=4 --master_port=29505 train.py \
 --cfg-path lavis/projects/instructblip/train/misclassifyqa/finetune_instructblip_misclassifyqa_64.yaml
 
 # Evaluation
-# python -m torch.distributed.run --nproc_per_node=2 --master_port=29505 train.py \
+# python -m torch.distributed.run --nproc_per_node=4 --master_port=29505 train.py \
 # --cfg-path lavis/projects/instructblip/train/misclassifyqa/finetune_instructblip_misclassifyqa_64_eval.yaml
