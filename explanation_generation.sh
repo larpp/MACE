3ebec044709f443cbc1c0511be2ea20648eb8d90
GyewanKim <87048326+larpp@users.noreply.github.com>
2026-06-30 16:14:28 +0900

    Update explanation_generation.sh

--- explanation_generation.sh
+++ explanation_generation.sh
@@ -1,7 +1,7 @@
 # Train
-CUDA_VISIBLE_DEVICES=0,1,2,3 python -m torch.distributed.run --nproc_per_node=2 --master_port=29505 train.py \
+CUDA_VISIBLE_DEVICES=0,1,2,3 python -m torch.distributed.run --nproc_per_node=4 --master_port=29505 train.py \
 --cfg-path lavis/projects/instructblip/train/misclassifyqa/finetune_instructblip_misclassifyqa_64_2.yaml
 
 # Evaluation
-# CUDA_VISIBLE_DEVICES=0,1,2,3 python -m torch.distributed.run --nproc_per_node=2 --master_port=29505 train.py \
+# CUDA_VISIBLE_DEVICES=0,1,2,3 python -m torch.distributed.run --nproc_per_node=4 --master_port=29505 train.py \
 # --cfg-path lavis/projects/instructblip/train/misclassifyqa/finetune_instructblip_misclassifyqa_64_2_eval.yaml
